7c0f28c17f764a9af6777f732d87c72ad39611a9
Emanuel-Lizarrondo <104107393+Emanuel-Lizarrondo@users.noreply.github.com>
2022-04-22 19:56:48 -0300

    Publicarfacenook nuevo

---
+++ src/ejercicio.ts
@@ -0,0 +1,7 @@
+console.log("Crear un posteo en facebook");
+
+console.log("Subir una foto");
+console.log("Etiquetar lugar");
+console.log("Etiquetar personas");
+console.log("Agregar una descripcion");
+console.log("Publicarla");
